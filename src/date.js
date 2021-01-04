71f42ab873a848fa9d4f62d83a9b8d24d635aa62
addo <addo.neto@gmail.com>
2021-01-04 16:44:00 -0300

    Quick fixes V1.0.1
    - Textarea autorisize fixed; - Date timezone fixed; - Article with no banner layout fixed

--- src/date.js
+++ src/date.js
@@ -1,7 +1,12 @@
 const months = [ 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
 'julho', 'agosto', 'setembro','outubro','novembro','dezembro'];
 
+//  Convert the Date stored in DB with US timestanp to Brazil GMT-3 format
 function ptFormat(d) {
-return `${d.getDate()} de ${months[d.getMonth()]} de ${d.getFullYear()} às ${d.toLocaleTimeString('pt-BR').slice(0,5)}`;}
+    const r = d.toLocaleString('pt-PT', {hour12: false, timezone : "America/Sao_Paulo"});
+    return `${r.slice(0,2)} de ${months[Number(r.slice(3,5))]} de ${r.slice(6,10)} às ${r.slice(12,17)}`;
+
+    //return `${d.getDate()} de ${months[d.getMonth()]} de ${d.getFullYear()} às ${d.toLocaleTimeString('pt-PT', {hour12: false}).slice(0,5)}`;
+}
 
 module.exports.ptFormat = ptFormat;
