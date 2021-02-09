e42172696b706f5f00e59a22239c0ecd67c77fc3
addo <addo.neto@gmail.com>
2021-02-08 21:55:28 -0300

    Date string error corrected

--- src/date.js
+++ src/date.js
@@ -4,7 +4,8 @@ const months = [ 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
 //  Convert the Date stored in DB with US timestanp to Brazil GMT-3 format
 function ptFormat(d) {
     const r = d.toLocaleString('pt-PT', {hour12: false, timezone : "America/Sao_Paulo"});
-    return `${r.slice(0,2)} de ${months[Number(r.slice(3,5))]} de ${r.slice(6,10)} às ${r.slice(12,17)}`;
+    // return `${r.slice(0,2)} de ${months[Number(r.slice(3,5))]} de ${r.slice(6,10)} às ${r.slice(12,17)}`;
+    return r;
 
     //return `${d.getDate()} de ${months[d.getMonth()]} de ${d.getFullYear()} às ${d.toLocaleTimeString('pt-PT', {hour12: false}).slice(0,5)}`;
 }
